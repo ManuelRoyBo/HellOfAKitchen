c4a7ee06269c0cf64830bfedc582e6ba51836d5d
kingpin1905 <48164038+kingpin1905@users.noreply.github.com>
2023-08-02 16:37:29 -0400

    redone item class structure for better readability

--- js/items.js
+++ js/items.js
@@ -1,5 +1,5 @@
 export class Item {
-    constructor(itemName, description, imageUrl, qrCodeId = null, cutEquivalent = null, grilledEquivalent = null, finalFormQrId = null, oddsOfBeingInABurger = null) {
+    constructor({ itemName, description, imageUrl, qrCodeId = null, cutEquivalent = null, grilledEquivalent = null, finalFormQrId = null, oddsOfBeingInABurger = null }) {
       this.itemName = itemName;
       this.description = description;
       this.imageUrl = imageUrl;
@@ -7,75 +7,64 @@ export class Item {
       this.cutEquivalent = cutEquivalent;
       this.grilledEquivalent = grilledEquivalent;
       this.finalFormQrId = finalFormQrId;
+      this.oddsOfBeingInABurger = oddsOfBeingInABurger;
     }
-  }
+}
   
-const cutLettuce = new Item(
-    "Cut lettuce",
-    "A piece of cut lettuce.",
-    "img/items/lettuce-leaf.svg",
-    null,
-    null,
-    null,
-    "CL",
-    0.5
-);
+const cutLettuce = new Item({
+    itemName: "Cut lettuce",
+    description: "A piece of cut lettuce.",
+    imageUrl: "img/items/lettuce-leaf.svg",
+    finalFormQrId: "CL",
+    oddsOfBeingInABurger: 0.5
+});
 
-const lettuce = new Item(
-    "Lettuce",
-    "A piece of lettuce.",
-    "img/items/lettuce.svg",
-    "IL",
-    cutLettuce
-);
+const lettuce = new Item({
+    itemName: "Lettuce",
+    description: "A piece of lettuce.",
+    imageUrl: "img/items/lettuce.svg",
+    qrCodeId: "IL",
+    cutEquivalent: cutLettuce
+});
 
-const cutTomato = new Item(
-    "Sliced tomato",
-    "A piece of cut tomato.",
-    "img/items/sliced-tomato.svg",
-    null,
-    null,
-    null,
-    "CT",
-    0.5
-);
+const cutTomato = new Item({
+    itemName: "Sliced tomato",
+    description: "A piece of cut tomato.",
+    imageUrl: "img/items/sliced-tomato.svg",
+    finalFormQrId: "CT",
+    oddsOfBeingInABurger: 0.5
+});
 
-const tomato = new Item(
-    "Tomato",
-    "A juicy red ball thingy.",
-    "img/items/tomato.svg",
-    "tomato",
-    cutTomato,
-);
+const tomato = new Item({
+    itemName: "Tomato",
+    description: "A juicy red ball thingy.",
+    imageUrl: "img/items/tomato.svg",
+    qrCodeId: "tomato",
+    cutEquivalent: cutTomato
+});
 
-const cookedGroundBeef = new Item(
-    "Cooked ground beef",
-    "Very tasty.",
-    "img/items/cooked-ground-beef.jpg",
-    null,
-    null,
-    null,
-    "CG",
-    0.9,
-);
+const cookedGroundBeef = new Item({
+    itemName: "Cooked ground beef",
+    description: "Very tasty.",
+    imageUrl: "img/items/cooked-ground-beef.jpg",
+    finalFormQrId: "CG",
+    oddsOfBeingInABurger: 0.9
+});
 
-const groundBeef = new Item(
-    "Ground beef",
-    "Can be grilled.",
-    "img/items/ground-beef.png",
-    null,
-    null,
-    cookedGroundBeef,
-);
+const groundBeef = new Item({
+    itemName: "Ground beef",
+    description: "Can be grilled.",
+    imageUrl: "img/items/ground-beef.png",
+    grilledEquivalent: cookedGroundBeef
+});
 
-const beef = new Item(
-    "Beef",
-    "Can be cut.",
-    "img/items/beef.svg",
-    "beef",
-    groundBeef,
-);
-  
+const beef = new Item({
+    itemName: "Beef",
+    description: "Can be cut.",
+    imageUrl: "img/items/beef.svg",
+    qrCodeId: "beef",
+    cutEquivalent: groundBeef
+});
+
 export const items = [lettuce, cutLettuce, tomato, cutTomato, beef, groundBeef, cookedGroundBeef];
-//TODO add ground beef and cooked ground beef
   
